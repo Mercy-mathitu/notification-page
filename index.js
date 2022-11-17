b1d1b9207cc5cd1090ef5acc33b09ed48a9b6e28
Mercy-mathitu <110403633+Mercy-mathitu@users.noreply.github.com>
2022-11-17 15:16:53 +0300

    added function for clicking on any of the unread

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 // basic js setup
-let unread = document.querySelectorAll("#unread")
+let unread = document.querySelectorAll(".unread")
 let markAll = document.getElementById("mark-all")
-let notifBtn = document.querySelector(".btn")
+let notification = document.querySelector(".btn")
 
-//mark all as read
+notification.textContent = unread.length
+
+
+
+// mark all as read
 markAll.addEventListener("click", () => {
     unread.forEach((unread) => {
         unread.classList.remove("unread")
     })
-    notifBtn.textContent = 0 
+   const newUnread = document.querySelectorAll(".unread")
+            notification.textContent = newUnread.length
 })
 
+ // click on any of the unread 
+unread.forEach((unread) => {
+        unread.addEventListener("click", () => {
+            unread.classList.remove("unread")
+            const newUnread = document.querySelectorAll(".unread")
+            notification.textContent = newUnread.length
+            
+        })
+    })
 
-// click on any of the unread
+ 
+
+
 
